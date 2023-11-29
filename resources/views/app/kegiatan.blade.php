1762437c102357cae1f067ec7f453ba7a2f2e3ca
ifs21046-itdel <riskianaomi17@gmail.com>
2023-11-29 15:51:02 +0700

    Updatekegiatan

--- resources/views/app/kegiatan.blade.php
+++ resources/views/app/kegiatan.blade.php
@@ -14,25 +14,8 @@
     <div class="site-section">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 co.l-lg-4 mb-5" data-aos="fade-up" data-aos-delay="100">
-                    <a href="single.html"><img src={{ asset('assets/images/temu22.jpg')}} alt="Image" class="img-fluid" /></a>
-                    <div class="p-4 bg-white">
-                        <span class="d-block text-secondary small text-uppercase">Feb 11th, 2023</span>
-                        <h2 class="h5 text-black mb-3">
-                            <a href="single.html">Welcoming Party untuk Angkatan 2022</a>
-                        </h2>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="200">
-                    <a href="single.html"><img src={{ asset('assets/images/temu22.jpg') }} class="img-fluid" /></a>
-                    <div class="p-4 bg-white">
-                        <span class="d-block text-secondary small text-uppercase">Nov 18th, 2023</span>
-                        <h2 class="h5 text-black mb-3">
-                            <a href="single.html">Temu Ramah Angkatan 2022 dengan 2023</a>
-                        </h2>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="300">
+
+            <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="300">
                     <a href="single.html"><img src={{ asset('assets/images/temu22.jpg') }} alt="Image" class="img-fluid" /></a>
                     <div class="p-4 bg-white">
                         <span class="d-block text-secondary small text-uppercase">Okt 28th, 2023</span>
@@ -60,12 +43,57 @@
                         </h2>
                     </div>
                 </div>
+                <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="100">
+                    <a href="single.html"><img src={{ asset('assets/images/temu22.JPG') }} alt="Image" class="img-fluid" /></a>
+                    <div class="p-4 bg-white">
+                        <span class="d-block text-secondary small text-uppercase">Apr 10th, 2023</span>
+                        <h2 class="h5 text-black mb-3">
+                            <a href="single.html">KM CUP</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="200">
+                    <a href="single.html"><img src={{ asset('assets/images/temu22.jpg')}} alt="Image" class="img-fluid" /></a>
+                    <div class="p-4 bg-white">
+                        <span class="d-block text-secondary small text-uppercase">Okt 24th, 2023</span>
+                        <h2 class="h5 text-black mb-3">
+                            <a href="single.html">KPU HIMASTI 2023/2024</a>
+                        </h2>
+                    </div>
+                </div>
                 <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="300">
                     <a href="single.html"><img src={{ asset('assets/images/temu22.jpg') }} class="img-fluid" /></a>
                     <div class="p-4 bg-white">
                         <span class="d-block text-secondary small text-uppercase">Sept 16th, 2023</span>
                         <h2 class="h5 text-black mb-3">
                             <a href="single.html">Sharing Knowledge 2023</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="200">
+                    <a href="single.html"><img src={{ asset('assets/images/temu22.jpg')}} alt="Image" class="img-fluid" /></a>
+                    <div class="p-4 bg-white">
+                        <span class="d-block text-secondary small text-uppercase">Okt 24th, 2023</span>
+                        <h2 class="h5 text-black mb-3">
+                            <a href="single.html">KPU HIMASTI 2023/2024</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="300">
+                    <a href="single.html"><img src={{ asset('assets/images/temu22.jpg') }} class="img-fluid" /></a>
+                    <div class="p-4 bg-white">
+                        <span class="d-block text-secondary small text-uppercase">Sept 16th, 2023</span>
+                        <h2 class="h5 text-black mb-3">
+                            <a href="single.html">Sharing Knowledge 2023</a>
+                        </h2>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up" data-aos-delay="200">
+                    <a href="single.html"><img src={{ asset('assets/images/temu22.jpg')}} alt="Image" class="img-fluid" /></a>
+                    <div class="p-4 bg-white">
+                        <span class="d-block text-secondary small text-uppercase">Okt 24th, 2023</span>
+                        <h2 class="h5 text-black mb-3">
+                            <a href="single.html">KPU HIMASTI 2023/2024</a>
                         </h2>
                     </div>
                 </div>
